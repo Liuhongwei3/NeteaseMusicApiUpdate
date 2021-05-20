9c431c4335f01cc66a67c4d954d450aa75365363
Liuhongwei3 <2873126657@qq.com>
2021-05-20 16:07:02 +0800

    remove unness files and update sth

--- module/event_forward.js
+++ module/event_forward.js
@@ -7,6 +7,7 @@ module.exports = (query, request) => {
     id: query.evId,
     eventUserId: query.uid,
   }
+  console.log(query)
   return request('POST', `https://music.163.com/weapi/event/forward`, data, {
     crypto: 'weapi',
     cookie: query.cookie,
--- module/personalized_privatecontent.js
+++ module/personalized_privatecontent.js
@@ -1,15 +1,15 @@
 // 独家放送
 
 module.exports = (query, request) => {
-  return request(
-    'POST',
-    `https://music.163.com/weapi/personalized/privatecontent`,
-    {},
-    {
-      crypto: 'weapi',
-      cookie: query.cookie,
-      proxy: query.proxy,
-      realIP: query.realIP,
-    },
-  )
-}
+    return request(
+        'POST',
+        `https://music.163.com/weapi/personalized/privatecontent`,
+        { limit: query.limit || 4 },
+        {
+            crypto: 'weapi',
+            cookie: query.cookie,
+            proxy: query.proxy,
+            realIP: query.realIP,
+        },
+    );
+};
